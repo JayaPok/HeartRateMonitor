a51253bd7731e8aea12dff3cf95a3c43d6b8c208
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-14 18:30:37 -0400

    wrote basic framework of code

---
+++ heart_rate_monitor.py
@@ -0,0 +1,20 @@
+def instaneous_heart_rate(signal):
+    """ estimate instanteous heart rate
+
+    :param signal: input signal from read_data()
+    :returns: instanteous heart rate
+    """
+    
+def avg_heart_rate():
+    """ estimate 1 and 5 min average heart rate
+
+	:param signal: input signal from read_data()
+	:returns: 1 and 5 min average heart rate
+	"""
+
+def read_data(filename = ""):
+    """ read in raw data from binary file
+
+	:param filename: default = ""
+	:returns: 
+	"""
