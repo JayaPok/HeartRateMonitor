8e87b9790bf9b536ed2839245f7581cadf0ba34a
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-21 23:06:04 -0400

    determined sample frequencies and arrays of ECG and Pleth

--- read_data.py
+++ read_data.py
@@ -1,8 +1,31 @@
-def read_data(filename = ""):
+def read_data(filename = "test.bin"):
     """ read in raw data from binary file
 
-	:param filename: default = ""
-	:returns: 
-	"""
+    :param filename: default = ""
+    :returns: """ 
+    import numpy as np
+    Freqs = np.fromfile(filename, dtype = 'uint16')
 
-	
+    ECGData = np.array(Freqs[::2])
+    PlethData = np.array(Freqs[1::2])
+    
+    ECGSampFreqHz = ECGData[0]
+    PlethSampFreqHz = PlethData[0]
+
+    return Freqs
+
+
+with open('test.bin', 'rb') as filename:
+    import numpy as np
+    Freqs = np.fromfile(filename, dtype = 'uint16')
+    print(Freqs)
+
+    ECGData = np.array(Freqs[::2])
+    PlethData = np.array(Freqs[1::2])
+    print(ECGData)
+    print(PlethData)
+    
+    ECGSampFreqHz = ECGData[0]
+    PlethSampFreqHz = PlethData[0]
+    print(ECGSampFreqHz)
+    print(PlethSampFreqHz)
