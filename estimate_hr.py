28150cd1c9a44bf269fb50db95def4d7b7871a61
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 16:53:49 -0400

    delete unused files

--- estimate_hr.py
+++
@@ -1,6 +0,0 @@
-def estimate_heart_rate(signal):
-    """ estimate instanteous heart rate
-
-    :param signal: input signal from read_data()
-    :returns: instanteous heart rate
-    """
--- process_hr.py
+++
@@ -1,6 +0,0 @@
-def avg_heart_rate():
-    """ estimate 1 and 5 min average heart rate
-
-	:param signal: input signal from read_data()
-	:returns: 1 and 5 min average heart rate
-	"""
