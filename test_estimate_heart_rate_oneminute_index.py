d806f643adc56e6c7d4604b02eca1af482770c25
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 20:27:33 -0400

    finished test for one minute heart rate with an input sine waveform

--- test_estimate_heart_rate_oneminute_index.py
+++ test_estimate_heart_rate_oneminute_index.py
@@ -3,8 +3,8 @@ from heart_rate_monitoring import heart_rate_indicies_Plethysmograph, estimate_h
 def test_estimate_heart_rate_oneminute_index():
     
     import numpy as np
-    Fs = 8000
-    sample = 8000
+    Fs = 80
+    sample = 50000
     f = 5
     x = np.arange(sample)
     y = np.array(np.sin(2 * np.pi * f * x / Fs))
@@ -12,4 +12,4 @@ def test_estimate_heart_rate_oneminute_index():
     instantaneous_HR_indicies_Pleth = heart_rate_indicies_Plethysmograph(y)
     one_minute_total_HR_Pleth = estimate_heart_rate_oneminute_index(instantaneous_HR_indicies_Pleth, Fs)
 
-    assert one_minute_total_HR_Pleth == [300, 300, 300]
+    assert one_minute_total_HR_Pleth == 300
