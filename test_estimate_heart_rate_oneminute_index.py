51d6aa5d8088db139b155b5c7a4714b4a703cede
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 21:48:38 -0400

    wrote docstrings for one minute heart rate test file

--- test_estimate_heart_rate_oneminute_index.py
+++ test_estimate_heart_rate_oneminute_index.py
@@ -1,7 +1,11 @@
 from heart_rate_monitoring import heart_rate_indicies_Plethysmograph, estimate_heart_rate_oneminute_index
 
 def test_estimate_heart_rate_oneminute_index():
-    
+    """ test estimate_heart_rate_oneminute_index() method
+
+    :param signal: input sine wave, and 0 indicies of peaks in sine wave
+    :returns: assertion of the last instantaneous one minute heart rate value of estimate_heart_rate_oneminute_index() method against the measured value
+    """
     import numpy as np
     Fs = 80
     sample = 50000
