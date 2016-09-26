72f889f68ba5118501b63227399cd0e397f43e89
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 20:32:09 -0400

    created test file in the correct format for five minute heart rate detection

---
+++ test_estimate_heart_rate_fiveminute_index.py
@@ -0,0 +1,15 @@
+from heart_rate_monitoring import heart_rate_indicies_Plethysmograph, estimate_heart_rate_fiveminute_index
+
+def test_estimate_heart_rate_fiveminute_index():
+    
+    import numpy as np
+    Fs = 80
+    sample = 50000
+    f = 5
+    x = np.arange(sample)
+    y = np.array(np.sin(2 * np.pi * f * x / Fs))
+
+    instantaneous_HR_indicies_Pleth = heart_rate_indicies_Plethysmograph(y)
+    one_minute_total_HR_Pleth = estimate_heart_rate_fiveminute_index(instantaneous_HR_indicies_Pleth, Fs)
+
+    assert one_minute_total_HR_Pleth == 300
