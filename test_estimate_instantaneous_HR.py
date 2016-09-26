f3b7ac2a3e64fc3a8f5a5a8eeb313aadc9b98405
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 21:50:08 -0400

    wrote docstrings for instantaneous heart rate test file

--- test_estimate_instantaneous_HR.py
+++ test_estimate_instantaneous_HR.py
@@ -1,6 +1,11 @@
 from heart_rate_monitoring import heart_rate_indicies_Plethysmograph, estimate_instantaneous_HR
 
 def test_estimate_instantaneous_HR():
+    """ test estimate_instantaneous_HR() method
+
+    :param signal: input sine wave, and 0 indicies of peaks in sine wave
+    :returns: assertion of the last instantaneous heart rate value of estimate_instantaneous_HR() method against the measured value
+    """
 
     import numpy as np
     Fs = 80
