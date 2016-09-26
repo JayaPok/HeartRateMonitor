056b9382c0b2a72a804aa1e00209693add6b3b29
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 20:01:55 -0400

    created a test file for the one minute heart rate calculations

---
+++ test_estimate_heart_rate_oneminute_index.py
@@ -0,0 +1,15 @@
+from heart_rate_monitoring import heart_rate_indicies_Plethysmograph, estimate_heart_rate_oneminute_index
+
+def test_estimate_heart_rate_oneminute_index():
+    
+    import numpy as np
+    Fs = 8000
+    sample = 8000
+    f = 5
+    x = np.arange(sample)
+    y = np.array(np.sin(2 * np.pi * f * x / Fs))
+
+    instantaneous_HR_indicies_Pleth = heart_rate_indicies_Plethysmograph(y)
+    one_minute_total_HR_Pleth = estimate_heart_rate_oneminute_index(instantaneous_HR_indicies_Pleth, Fs)
+
+    assert one_minute_total_HR_Pleth == [300, 300, 300]
