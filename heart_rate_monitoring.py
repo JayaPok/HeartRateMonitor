04c55ed9406856de5fd8b185a6c5995359f49cde
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-09-25 23:42:46 -0400

    removed rest of improperly formated test cases

--- heart_rate_monitoring.py
+++ heart_rate_monitoring.py
@@ -78,7 +78,7 @@ def estimate_instantaneous_HR(instantaneous_HR_indicies_Pleth, PlethSampFreqHz):
                     print("Bradycardia alert! Here is 10 minute trace of heart rate:")
                     return ten_min_real_Pleth
                     break
-        if instantaenous_HR_Pleth > 240: # detects for Bradycardia where heart rate is above 240 bpm
+        elif instantaenous_HR_Pleth > 240: # detects for Bradycardia where heart rate is above 240 bpm
             invertedHR_Pleth = np.array(all_HR_Pleth[::-1])
             invertedHRseconds_Pleth = 1 / (invertedHR_Pleth / 60)
             HR_sum_Pleth = 0
@@ -90,7 +90,8 @@ def estimate_instantaneous_HR(instantaneous_HR_indicies_Pleth, PlethSampFreqHz):
                     print("Tachycardia alert! Here is 10 minute trace of heart rate:")
                     return ten_min_real_Pleth
                     break
-        return instantaenous_HR_Pleth
+        else:
+            return all_HR_Pleth
         k+=1        
 
     
@@ -101,6 +102,7 @@ def estimate_heart_rate_oneminute_index(instantaneous_HR_indicies_Pleth, PlethSa
     :returns: one minute average heart rate
     """
     one_minute_HR_Pleth = []
+    total_one_minute_HR_pleth = []
     k = 0
     while k < len(instantaneous_HR_indicies_Pleth)-1:
         time_between_beats_Pleth = (instantaneous_HR_indicies_Pleth[k+1] - instantaneous_HR_indicies_Pleth[k]) / PlethSampFreqHz
@@ -108,8 +110,9 @@ def estimate_heart_rate_oneminute_index(instantaneous_HR_indicies_Pleth, PlethSa
         one_minute_HR_sum_Pleth = sum(one_minute_HR_Pleth)
         if one_minute_HR_sum_Pleth > 60:
             one_minute_total_HR_Pleth =  len(one_minute_HR_Pleth)
+            total_one_minute_HR_pleth.append(one_minute_total_HR_Pleth)
             one_minute_HR_Pleth = []
-            return one_minute_total_HR_Pleth
+            return total_one_minute_HR_pleth
         k+=1
     
 
@@ -121,6 +124,7 @@ def estimate_heart_rate_fiveminute_index(instantaneous_HR_indicies_Pleth, PlethS
     """
 
     five_minute_HR_Pleth = []
+    total_five_minute_HR_pleth = []
     k = 0
     while k < len(instantaneous_HR_indicies_Pleth)-1:
         time_between_beats_Pleth = (instantaneous_HR_indicies_Pleth[k+1] - instantaneous_HR_indicies_Pleth[k]) / PlethSampFreqHz
@@ -128,164 +132,9 @@ def estimate_heart_rate_fiveminute_index(instantaneous_HR_indicies_Pleth, PlethS
         five_minute_HR_sum_Pleth = sum(five_minute_HR_Pleth)
         if five_minute_HR_sum_Pleth > 300:
             five_minute_total_HR_Pleth =  len(five_minute_HR_Pleth) / 5
+            total_five_minute_HR_pleth.append(five_minute_total_HR_Pleth)
             five_minute_HR_Pleth = []
-            return five_minute_total_HR_Pleth
+            return total_five_minute_HR_pleth
         k+=1
 
-
-def test_instantaneous_HR():
-
-    import numpy as np
-    Fs = 8000
-    sample = 8000
-    f = 5
-    x = np.arange(sample)
-    y = np.array(np.sin(2 * np.pi * f * x / Fs))
-    a = 0
-
-    instantaneous_HR_indicies = [] # Array which holds temporary values of heart rates as data is read
-    i=6
-    while i < y.size-6:
-        #if y[i] > np.mean(y[i-1:i-5]) and y[i] > np.mean(y[i+1:i+5]):
-        if y[i] > y[i-1] and y[i] > y[i+1]:
-            instantaneous_HR_indicies.append(i)
-            #print(y[i])
-        i+=1
-    # assert instantaneous_HR_indicies == [1, 1, 1, 1, 1]
-
-
-    one_minute_HR = []
-    k = 0
-    while k < len(instantaneous_HR_indicies)-1:
-        time_between_beats = (instantaneous_HR_indicies[k+1] - instantaneous_HR_indicies[k]) / Fs
-        one_minute_HR.append(time_between_beats)
-        one_minute_HR_sum = sum(one_minute_HR)
-        if one_minute_HR_sum > 300:
-            one_minute_total_HR = len(one_minute_HR) / 5
-            one_minute_HR = []
-            print(one_minute_total_HR)
-        k+=1
-
-    #print(instantaneous_HR_indicies)
-    all_HR = []
-    k = 0
-    while k < len(instantaneous_HR_indicies)-1:
-        instantaenous_HR = ((60 * Fs) / (instantaneous_HR_indicies[k+1] - instantaneous_HR_indicies[k])) # calculates instantaenous HR for each beat
-        all_HR.append(instantaenous_HR)
-        print(instantaenous_HR)
-        if instantaenous_HR < 30: # detects for Bradycardia where heart rate is below 30 bpm
-            #print("Bradycardia alert! Here is 10 minute trace of heart rate:")
-            invertedHR = np.array(all_HR[::-1])
-            invertedHRseconds = 1 / (invertedHR / 60)
-            HR_sum = 0
-            for x in range(0, k):
-                HR_sum = HR_sum + invertedHRseconds[x]
-                ten_min_invert = invertedHR[0:x]
-                ten_min_real = np.array(ten_min_invert[::-1])
-                if np.sum(ten_min_real) > 600:
-                    print(ten_min_real)
-                    break
-        if instantaenous_HR > 240: # detects for Bradycardia where heart rate is above 240 bpm
-            #print("Tachycardia alert! Here is 10 minute trace of heart rate:")
-            invertedHR = np.array(all_HR[::-1])
-            invertedHRseconds = 1 / (invertedHR / 60)
-            HR_sum = 0
-            for x in range(0, k):
-                HR_sum = HR_sum + invertedHRseconds[x]
-                ten_min_invert = invertedHR[0:x]
-                ten_min_real = np.array(ten_min_invert[::-1])
-                if np.sum(ten_min_real) > 600:
-                    print(ten_min_real)
-                    break
-        k+=1
-
-test_instantaneous_HR()
-
-
-def test_estimate_heart_rate():
-    read_data()    
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    Fs = 20
-    sample = 5000
-    f = 3
-    x = np.arange(sample)
-    y = np.array(np.sin(2 * np.pi * f * x / Fs))
-    #print(y)
-
-    inst_samples = 5*Fs
-
-   
-    #plt.plot(x/Fs,y)
-    #plt.show()
-
-    # maxvalues_one = []
-    # maxvalues_one_array = []
-    # i = 0
-    # while i < y.size-1:
-    #     if y[i] > y[i-1] and y[i] > y[i+1]:
-    #         maxvalues_one.append(y[i])
-    #     if i%(60*Fs) == 0:
-    #         maxvaluesarray_one = np.array(maxvalues_one)
-    #         maxvalues_one_array.append(maxvaluesarray_one.size)
-    #         #print(one_min_avg)
-    #         #return one_min_avg
-    #         maxvalues_one = []
-
-    #     i+=1
-
-    # maxvalues_five = []
-    # maxvalues_five_array = []
-    # i = 0
-    # while i < y.size-1:
-    #     if y[i] > y[i-1] and y[i] > y[i+1]:
-    #         maxvalues_five.append(y[i])
-    #     if i%(300*Fs) == 0:
-    #         maxvaluesarray_five = np.array(maxvalues_five)
-    #         maxvalues_five_array.append(maxvaluesarray_five.size / 5)
-    #         #print(five_min_avg)
-    #         #return five_min_avg
-    #         maxvalues_five = []
-
-    #     i+=1
-    
-    # print(maxvalues_one_array)
-
-
-    # instantaneous_HR_temp = [] # Array which holds temporary values of heart rates as data is read
-    # instantaenous_HR_TenMin = []
-    # i=0
-    # while i < y.size-1:
-    #     if y[i] > y[i-1] and y[i] > y[i+1]:
-    #         instantaneous_HR_temp.append(y[i])
-    #     if i % (2 * Fs) == 0: # Identifies everytime 2 seconds of data has passed
-    #         instantaneous_HR_array = np.array(instantaneous_HR_temp)
-    #         instantaenous_HR = instantaneous_HR_array.size * 30
-    #         instantaenous_HR_TenMin.append(instantaenous_HR)
-    #         print(instantaenous_HR)
-    #         if instantaenous_HR < 30:
-    #             print("Bradycardia alert!")
-    #         if instantaenous_HR > 240:
-    #             print("Tachycardia alert!")
-    #     #if i > (600 * ECGSampFreqHz):
-    #         #instantaenous_HR_TenMin = instantaenous_HR_TenMin[1:len(instantaenous_HR_TenMin)]
-    #     i+=1 
-
-
-#test_estimate_heart_rate()
-
-def output_instaneous_heart_rate(signal):
-    """ output estimate instanteous heart rate
-
-    :param signal: input signal from read_data()
-    :returns: instanteous heart rate
-    """
-
-def output_avg_heart_rate():
-    """ output estimate 1 and 5 min average heart rate
-
-    :param signal: input signal from read_data()
-    :returns: 1 and 5 min average heart rate
-    """
-
+  
